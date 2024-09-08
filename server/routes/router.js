d56cdda7ab0731fb3d859a0b5dcb3638dda07786
RonyEshel <eshelrony@gmail.com>
2024-09-08 08:53:12 +0300

    adding the hosts moudle, controller and service

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,6 +3,8 @@ const route = express.Router();
 
 const services = require('../services/render');
 const customersController = require('../controllers/customersController');
+const hostController = require('../controllers/hostController');  
+
 
 // Customers
 route.get('/api/customers/:id', customersController.getId);
@@ -11,4 +13,13 @@ route.post('/api/customers/:id', customersController.update);
 route.post('/api/customers', customersController.create);
 route.delete('/api/customers/:id', customersController.delete);
 
+// Hosts routes
+route.get('/api/hosts/:id', hostController.getHostById);  // Get a specific host by ID
+route.get('/api/hosts', hostController.listHosts);        // List all hosts
+route.post('/api/hosts/:id', hostController.updateHost);  // Update a specific host by ID
+route.post('/api/hosts', hostController.createHost);      // Create a new host
+route.delete('/api/hosts/:id', hostController.deleteHost);  // Delete a specific host by ID
+
 module.exports = route;
+
+
