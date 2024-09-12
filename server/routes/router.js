7b5f5b973c5ac96fa38a9e6d751e3973ecf428e3
eitan5001100 <eitan5001100@gmail.com>
2024-09-12 20:33:35 +0300

    Apartments

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,6 +3,7 @@ const route = express.Router();
 
 const services = require('../services/render');
 const customersController = require('../controllers/customersController');
+const apartmentsController = require('../controllers/apartmentsController');
 
 // Customers
 route.get('/api/customers/:id', customersController.getId);
@@ -10,5 +11,12 @@ route.get('/api/customers', customersController.get);
 route.post('/api/customers/:id', customersController.update);
 route.post('/api/customers', customersController.create);
 route.delete('/api/customers/:id', customersController.delete);
+
+// Apartments
+route.get('/api/apartments/:id', apartmentsController.getApartmentId); // Get a specific Apartment by ID
+route.get('/api/apartments', apartmentsController.getApartments); // List all apartments
+route.post('/api/apartments/:id', apartmentsController.updateApartment); // Update a specific apartment by ID
+route.post('/api/apartments', apartmentsController.createApartment); // Create a new apartment
+route.delete('/api/apartments/:id', apartmentsController.deleteApartment); // Delete a specific apartment by ID
 
 module.exports = route;
