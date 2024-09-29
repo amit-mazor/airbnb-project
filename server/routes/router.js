e03e73fc24f444853a6a2e3e022d15cb135aa3dc
amit-mazor <amitmazor37@gmail.com>
2024-09-29 22:50:39 +0300

    update customers controller, model, and routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,37 +1,47 @@
-const express = require('express');
-const route = express.Router();
-
-const services = require('../services/render');
-const customersController = require('../controllers/customersController');
-
-const apartmentsController = require('../controllers/apartmentsController');
-const hostController = require('../controllers/hostController');  
+// SPLIT BY COMPONENT
 
 
-// Customers
-route.get('/api/customers/:id', customersController.getId);
-route.get('/api/customers', customersController.get);
-route.post('/api/customers/:id', customersController.update);
-route.post('/api/customers', customersController.create);
-route.delete('/api/customers/:id', customersController.delete);
 
 
-// Apartments
-route.get('/api/apartments/:id', apartmentsController.getApartmentId); // Get a specific Apartment by ID
-route.get('/api/apartments', apartmentsController.getApartments); // List all apartments
-route.post('/api/apartments/:id', apartmentsController.updateApartment); // Update a specific apartment by ID
-route.post('/api/apartments', apartmentsController.createApartment); // Create a new apartment
-route.delete('/api/apartments/:id', apartmentsController.deleteApartment); // Delete a specific apartment by ID
 
 
-// Hosts routes
-route.get('/api/hosts/:id', hostController.getHostById);  // Get a specific host by ID
-route.get('/api/hosts', hostController.listHosts);        // List all hosts
-route.post('/api/hosts/:id', hostController.updateHost);  // Update a specific host by ID
-route.post('/api/hosts', hostController.createHost);      // Create a new host
-route.delete('/api/hosts/:id', hostController.deleteHost);  // Delete a specific host by ID
 
-module.exports = route;
+
+
+// const express = require('express');
+// const route = express.Router();
+
+// const services = require('../services/render');
+// const customersController = require('../controllers/customersController');
+
+// const apartmentsController = require('../controllers/apartmentsController');
+// const hostController = require('../controllers/hostController');  
+
+
+// // Customers
+// route.get('/api/customers/:id', customersController.getId);
+// route.get('/api/customers', customersController.get);
+// route.post('/api/customers/:id', customersController.update);
+// route.post('/api/customers', customersController.create);
+// route.delete('/api/customers/:id', customersController.delete);
+
+
+// // Apartments
+// route.get('/api/apartments/:id', apartmentsController.getApartmentId); // Get a specific Apartment by ID
+// route.get('/api/apartments', apartmentsController.getApartments); // List all apartments
+// route.post('/api/apartments/:id', apartmentsController.updateApartment); // Update a specific apartment by ID
+// route.post('/api/apartments', apartmentsController.createApartment); // Create a new apartment
+// route.delete('/api/apartments/:id', apartmentsController.deleteApartment); // Delete a specific apartment by ID
+
+
+// // Hosts routes
+// route.get('/api/hosts/:id', hostController.getHostById);  // Get a specific host by ID
+// route.get('/api/hosts', hostController.listHosts);        // List all hosts
+// route.post('/api/hosts/:id', hostController.updateHost);  // Update a specific host by ID
+// route.post('/api/hosts', hostController.createHost);      // Create a new host
+// route.delete('/api/hosts/:id', hostController.deleteHost);  // Delete a specific host by ID
+
+// module.exports = route;
 
 
 
