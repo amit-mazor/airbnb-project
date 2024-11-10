9931b25efc4317d882f8fc8077cdf598fea3145c
Nir <thenirtshuva@gmail.com>
2024-11-10 22:08:24 +0200

    apartment update for host

--- server/routes/apartmentRoutes.js
+++ server/routes/apartmentRoutes.js
@@ -5,7 +5,7 @@ const apartmentController = require('../controllers/apartmentController');
 // Define routes for apartment CRUD operations and search
 router.get('/', apartmentController.getAllApartments);            // GET /api/apartments - Get all apartments or search
 router.post('/', apartmentController.createApartment);            // POST /api/apartments - Create a new apartment
-router.put('/:id', apartmentController.updateApartment);          // PUT /api/apartments/:id - Update apartment by ID
+router.post('/update/:id', apartmentController.updateApartment);          // PUT /api/apartments/:id - Update apartment by ID
 router.delete('/:id', apartmentController.deleteApartment);       // DELETE /api/apartments/:id - Delete apartment by ID
 router.get('/host/:username', apartmentController.getHostApartments);  // GET /api/apartments/host/:username - Get all apartments of a specific host 
 router.get('/search', apartmentController.searchApartments);
